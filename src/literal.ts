245209aea0cede981306d4e0a55e9e690e1065f1
matsuko <atsuko_mouri@heartcore.co.jp>
2024-02-16 17:31:15 +0900

    add literal.ts

---
+++ src/literal.ts
@@ -0,0 +1,17 @@
+export{};
+
+let dayOfTheWeek: '日' |'月' |'火' |'水' |'木' |'金' |'土';
+dayOfTheWeek = '月'
+// dayOfTheWeek = '31'
+
+let month: 1|2|3|4|5|6|7|8|9|10|11|12;
+month = 12;
+// month = 13;
+
+let trueTrue: true  = true;
+trueTrue = false;
+
+
+//プリミティブ型よりさらに細かい指定ができる
+//３つの型がある→文字列・数値・真偽値
+//真偽値については文法上機能はあるが、実質的な利用シーンは特段ない
