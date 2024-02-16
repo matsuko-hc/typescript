42d036f255caf03f9059b00c3f0036ac77ee0e51
matsuko <atsuko_mouri@heartcore.co.jp>
2024-02-16 11:14:45 +0900

    add null-undefind

--- src/boid.ts
+++ src/boid.ts
@@ -5,3 +5,8 @@ function returnNothing(): void{
 }
 
 console.log(returnNothing());
+
+//概念のお話
+//void型は関数が何も返さないことを示す型
+//undefindは関数が明示的に何も返さない、（戻り値がない）ことを意味する
+//ただvoid = undefind ではない。
---
+++ src/null-undefind.ts
@@ -0,0 +1,9 @@
+export{};
+
+let absence: null = null;
+// absence = 'Hello'
+
+// let data: undefind = undefind;
+//なぜかエラーになる
+//実際の案件ではあまり使用しないらしい
+//とりあえずこういう型があるんだなと知っておけばOK
